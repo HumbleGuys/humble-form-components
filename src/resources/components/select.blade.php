cb0588c100ec87a96bd7b0e045bbefdeedd99642
Kim Larsson <kim@humbleguys.se>
2022-09-04 19:30:13 +0200

    Improved css

--- src/resources/components/select.blade.php
+++ src/resources/components/select.blade.php
@@ -6,28 +6,32 @@
     'optionValue' => 'value',
 ])
 
-<select
-    id="{{ $name }}"
-    name="{{ $name }}"
-    {{ $attributes->merge(['class' => 'input select']) }}
-    @if (!$attributes->has('x-model'))
-        x-model="formData.{{ $name }}"
-    @endif
->
-    @if ($placeholder)
-        <option 
-            value=""
-            disabled
-            hidden
-            selected    
-        >
-            {{ $placeholder }}
-        </option>
-    @endif
+<div class="inputHolder">
+    <select
+        id="{{ $name }}"
+        name="{{ $name }}"
+        {{ $attributes->merge(['class' => 'input select']) }}
+        @if (!$attributes->has('x-model'))
+            x-model="formData.{{ $name }}"
+        @endif
+    >
+        @if ($placeholder)
+            <option 
+                value=""
+                disabled
+                hidden
+                selected    
+            >
+                {{ $placeholder }}
+            </option>
+        @endif
 
-    @foreach ($items as $item)
-        <option value="{{ is_array($item) ? $item[$optionValue] : $item->{$optionValue} }}">
-            {{ is_array($item) ? $item[$optionLabel] : $item->{$optionLabel} }}
-        </option>
-    @endforeach
-</select>
+        @foreach ($items as $item)
+            <option value="{{ is_array($item) ? $item[$optionValue] : $item->{$optionValue} }}">
+                {{ is_array($item) ? $item[$optionLabel] : $item->{$optionLabel} }}
+            </option>
+        @endforeach
+    </select>
+
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="select__chevron"><polyline points="6 9 12 15 18 9"></polyline></svg>
+</div>
--- src/resources/components/textarea.blade.php
+++ src/resources/components/textarea.blade.php
@@ -2,11 +2,13 @@
     'name',
 ])
 
-<textarea 
-    id="{{ $name }}"
-    name="{{ $name }}"
-    {{ $attributes->merge(['class' => 'textarea input']) }}
-    @if (!$attributes->has('x-model'))
-        x-model="formData.{{ $name }}"
-    @endif
-></textarea>
+<div class="inputHolder">
+    <textarea 
+        id="{{ $name }}"
+        name="{{ $name }}"
+        {{ $attributes->merge(['class' => 'textarea input']) }}
+        @if (!$attributes->has('x-model'))
+            x-model="formData.{{ $name }}"
+        @endif
+    ></textarea>
+</div>
