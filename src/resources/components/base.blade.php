302d9a02f3bce629182d5a0d61d3eeb519b91f44
Kim Larsson <kim@humbleguys.se>
2022-09-05 13:14:57 +0200

    Prepared for version 0.0.1 relaease

--- src/resources/components/base.blade.php
+++ src/resources/components/base.blade.php
@@ -17,3 +17,10 @@
 >
     {!! $slot !!}
 </form>
+
+@once
+    @push('head')
+        <link rel="stylesheet" href="{{ asset('../vendor/humble-guys/humble-form-components/public/resources/dist/assets/index.11d3527d.css') }}">
+        <script module defer src="{{ asset('../vendor/humble-guys/humble-form-components/public/resources/dist/assets/index.390e90d9.js') }}"></script>
+    @endpush   
+@endonce 
