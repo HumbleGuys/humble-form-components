ceb2172eed981b85e6fef5b1f40910a3b7d83a8f
Henric Åkesson <henric@bravissimo.se>
2022-11-01 11:40:06 +0100

    Glömt uppdatera versionen på filerna.

--- src/resources/components/base.blade.php
+++ src/resources/components/base.blade.php
@@ -22,11 +22,11 @@
 
 @once
     @push('head')
-        <link rel="stylesheet" href="{{ asset('../vendor/humble-guys/humble-form-components/public/resources/dist/style.css?v=0.0.10') }}">
-        <script module defer src="{{ asset('../vendor/humble-guys/humble-form-components/public/resources/dist/humble-form-components.umd.js?v=0.0.10') }}"></script>
-    
+        <link rel="stylesheet" href="{{ asset('../vendor/humble-guys/humble-form-components/public/resources/dist/style.css?v=0.0.15') }}">
+        <script module defer src="{{ asset('../vendor/humble-guys/humble-form-components/public/resources/dist/humble-form-components.umd.js?v=0.0.15') }}"></script>
+
         @if (!empty($recaptcha))
             <script src="https://www.google.com/recaptcha/api.js?render={{ $recaptcha }}"></script>
         @endif
     @endpush
-@endonce 
+@endonce
